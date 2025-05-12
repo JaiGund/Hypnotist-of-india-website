1e5e777d24f6c00f9168b9baf44b1b56df5ce9c6
JaiGund <jailaxmangund@gmail.com>
2025-05-12 14:40:05 +0530

    Switched database to mongoDB atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectToDb = async () => {
-  await mongoose
-    .connect("mongodb://localhost:27017/hypnotism")
-    .then(()=>console.log("Connected to DB"));
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB Atlas");
+  } catch (err) {
+    console.error("DB connection error:", err);
+  }
 };
